458da9aee912a8da8e215fe92df5e484675637bf
Ramya Kulkarni <ramyakulkarni@Ramyas-MacBook-Air.local>
2024-07-31 13:05:13 +0530

    Added collections Programs

---
+++ src/main/java/interview_programs/HashMapExample.java
@@ -0,0 +1,33 @@
+package interview_programs;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        // In hashmap for key only 1 null key is allowed. for value, multiple null values are allowed.
+        HashMap<String,String> hm = new HashMap<>(); //dclr.
+        hm.put("username","value1");//assignment
+        hm.put("user","value2");
+        hm.put("usr","");//null val
+        hm.put("","value4"); //1 null key
+        hm.put("ggg",""); // null val
+
+        Hashtable<String,String> ht = new Hashtable<>();
+        ht.put("","value1");
+        ht.put("","value5");
+        ht.put("aaa","value4");
+        ht.put("user3","");
+        ht.put("stuer2","");
+        System.out.println(ht);
+
+
+
+        //System.out.println(hm);
+
+        for(Map.Entry m: hm.entrySet()){
+            System.out.println(m.getKey()+" , "+m.getValue());
+        }
+    }
+}
---
+++ src/main/java/interview_programs/SetExample.java
@@ -0,0 +1,26 @@
+package interview_programs;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetExample {
+    public static void main(String[] args)
+    {
+        // Set demonstration using HashSet
+        Set<String> hash_Set = new HashSet<String>();
+        hash_Set.add("Geeks");
+        hash_Set.add("For");
+        hash_Set.add("Geeks");
+        hash_Set.add("Example");
+        hash_Set.add("Set");
+        System.out.print("Set output without the duplicates\n");
+
+        System.out.println(hash_Set);
+
+        // Set demonstration using TreeSet
+        System.out.print("Sorted Set after passing into TreeSet\n");
+        Set<String> tree_Set = new TreeSet<String>(hash_Set);
+        System.out.println(tree_Set);
+    }
+}
